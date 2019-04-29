5c96af76500f6f8b533d533bf234c671d43c1be8
singein <singein@outlook.com>
2019-04-29 14:06:44 +0800

    added folder static

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-index_html_path = os.path.join(BASE_DIR, 'index.html')
+index_html_path = os.path.join(BASE_DIR, 'static', 'index.html')
